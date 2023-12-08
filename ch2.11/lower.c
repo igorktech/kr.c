06e52c3f0d4739d7e20e813701b97f0cd6d31d2f
Igor <igoranchikkuzmin@yandex.ru>
2023-12-09 00:26:42 +0100

    feat(master): second chapter

---
+++ ch2.11/lower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int lower(int c);
+
+int main(){
+    char  character = 'T';
+    printf("Original character: %c, converted to lower case: %c\n", character, lower(character));
+    return 0;
+}
+
+int lower(int c)
+{
+   return (c>= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
